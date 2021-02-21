435839b4a7582510e5f81d22675c08c944f7f83b
Angelescu Violeta <violeta@girlsincode.com>
2021-02-21 22:24:08 +0100

    routes, env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Characters from './components/Characters/Characters';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import store from './state/store';
+import { PrivateRoute } from './utils/PrivateRoute';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        hollaa
-      </header>
-    </div>
+    <Provider store={store}>
+      <h1>Rick and Morty Characters</h1>
+      <Router>
+        <Switch>
+          <PrivateRoute exact component={Characters} path='/' />
+          <Route exact component={Login} path='/login' />
+          <Route exact component={Register} path='/register' />
+          <Route path='*'>
+            <Redirect to='/' />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
